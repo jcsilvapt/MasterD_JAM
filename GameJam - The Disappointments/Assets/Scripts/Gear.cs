4a2b2d404011f761aaea4e1bbde60ad5d0120754
DarkKool <43683250+DarkKool@users.noreply.github.com>
2021-05-05 16:00:00 +0100

    Dead Animations & Throw Animations

---
+++ GameJam - The Disappointments/Assets/Scripts/Gear.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        isActive = true;
+        transform.eulerAngles = Vector3.zero;
+        transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.eulerAngles += new Vector3(0, 2f, 0) * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            rb.isKinematic = true;
+            isActive = false;
+        }
+    }
+    /*{
+        if(other.tag != "Player")
+        {
+            rb.isKinematic = true;
+            isActive = false;
+        }
+    }
+    */
+}
--- GameJam - The Disappointments/Assets/Scripts/PlayerController.cs
+++ GameJam - The Disappointments/Assets/Scripts/PlayerController.cs
@@ -30,7 +30,12 @@ public class PlayerController : MonoBehaviour
         ["RunRight"] = "Run_Right",
         ["JumpInPlace"] = "Jump_InPlace",
         ["JumpLeft"] = "Jump_Left",
-        ["JumpRight"] = "Jump_Right"
+        ["JumpRight"] = "Jump_Right",
+        ["DieInPlace"] = "Die_InPlace",
+        ["DieLeft"] = "Die_Left",
+        ["DieRight"] = "Die_Right",
+        ["ThrowLeft"] = "Throw_Left",
+        ["ThrowRight"] = "Throw_Right"
     };
 
     //Holds the Current Animation State
@@ -39,12 +44,25 @@ public class PlayerController : MonoBehaviour
     //Speed Array
     [SerializeField] private float[] speedSet;
 
+    //Flag Controlling if Player got Hit
+    private bool hit;
+
+    //Flag Controlling if Player's Alive
+    private bool isAlive;
+
+    //Weapon Reference
+    [SerializeField] private Transform gear;
+
     // Start is called before the first frame update
     void Start()
     {
         //Get References
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+
+        //Set Flag
+        hit = false;
+        isAlive = true;
     }
 
     // Update is called once per frame
@@ -60,78 +78,125 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            transform.position = new Vector3(0, .5f, 0);
+            transform.position = new Vector3(0.5f, .01f, 0);
+            hit = false;
+            isAlive = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            hit = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ChangeAnimationState(AnimationStates["ThrowLeft"]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ChangeAnimationState(AnimationStates["ThrowRight"]);
         }
     }
 
     private void FixedUpdate()
     {
-        //Check if Player in on the Ground
-        if (Physics.Raycast(transform.position, Vector3.down, rayDistance))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
-        //Check Movement Based on Input
         Vector3 velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
-        if (movement.x < 0)
-        {
-            velocity.x = -speedSet[1];
-        }
-        else if (movement.x > 0)
-        {
-            velocity.x = speedSet[1];
-        }
-        else
-        {
-            velocity.x = speedSet[0];
-        }
 
-        //Check if Trying to Jump
-        if (jump && isGrounded)
+        if (!hit)
         {
-            rb.AddForce(Vector3.up * 1000, ForceMode.Force);
-            jump = false;
-        }
+            //Check if Player in on the Ground
+            if (Physics.Raycast(transform.position, Vector3.down, rayDistance))
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
 
-        //Animation Control
-        if (isGrounded)
-        {
+            //Check Movement Based on Input
             if (movement.x < 0)
             {
-                ChangeAnimationState(AnimationStates["RunLeft"]);
+                velocity.x = -speedSet[1];
             }
             else if (movement.x > 0)
             {
-                ChangeAnimationState(AnimationStates["RunRight"]);
+                velocity.x = speedSet[1];
             }
             else
             {
-                ChangeAnimationState(AnimationStates["Idle"]);
+                velocity.x = speedSet[0];
             }
+
+            //Check if Trying to Jump
+            if (jump && isGrounded)
+            {
+                rb.AddForce(Vector3.up * 1000, ForceMode.Force);
+                jump = false;
+            }
+        
+
+            //Animation Control
+            if (isGrounded)
+            {
+                if (movement.x < 0)
+                {
+                    ChangeAnimationState(AnimationStates["RunLeft"]);
+                }
+                else if (movement.x > 0)
+                {
+                    ChangeAnimationState(AnimationStates["RunRight"]);
+                }
+                else
+                {
+                    ChangeAnimationState(AnimationStates["Idle"]);
+                }
+            }
+            else
+            {
+                if(movement.x < 0)
+                {
+                    ChangeAnimationState(AnimationStates["JumpLeft"]);
+                }
+                else if (movement.x > 0)
+                {
+                    ChangeAnimationState(AnimationStates["JumpRight"]);
+                }
+                else
+                {
+                    ChangeAnimationState(AnimationStates["JumpInPlace"]);
+                }
+            }
+
+            //Assign new Velocity to Rigidbody
+            rb.velocity = velocity;
         }
         else
         {
-            if(movement.x < 0)
+            if (isAlive)
             {
-                ChangeAnimationState(AnimationStates["JumpLeft"]);
-            }
-            else if (movement.x > 0)
-            {
-                ChangeAnimationState(AnimationStates["JumpRight"]);
-            }
-            else
-            {
-                ChangeAnimationState(AnimationStates["JumpInPlace"]);
+                if (movement.x < 0)
+                {
+                    ChangeAnimationState(AnimationStates["DieLeft"]);
+                }
+                else if (movement.x > 0)
+                {
+                    ChangeAnimationState(AnimationStates["DieRight"]);
+                }
+                else
+                {
+                    ChangeAnimationState(AnimationStates["DieInPlace"]);
+                }
+                isAlive = false;
             }
         }
 
-        //Assign new Velocity to Rigidbody
-        rb.velocity = velocity;
+        if (!isAlive)
+        {
+            velocity.x = 0;
+            rb.velocity = velocity;
+        }
     }
 
     #region Animator Controller Methods
@@ -148,6 +213,28 @@ public class PlayerController : MonoBehaviour
 
         //Reassign the current state
         currentState = newState;
+    }
+    #endregion
+
+    #region Animation Events
+    public void Throw()
+    {
+        Rigidbody gearRB = gear.GetComponent<Rigidbody>();
+        gearRB.isKinematic = false;
+        gear.GetComponent<BoxCollider>().enabled = true;
+        gear.GetComponent<Gear>().enabled = true;
+        gear.SetParent(null);
+        gearRB.AddForce(transform.right * 10f, ForceMode.Impulse);
+
+        Debug.Log("Working " + gear);
+
+    }
+
+    public void FinishedAnimation()
+    {
+        Debug.Log("Works");
+
+        ChangeAnimationState(AnimationStates["Idle"]);
     }
     #endregion
 }
