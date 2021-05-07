6c938d9ef18698a02f4900c3c48121d0f6dbae9a
DarkKool <43683250+DarkKool@users.noreply.github.com>
2021-05-07 03:10:29 +0100

    InGame UI's (Victory, Defeat and Pause) & TutorialTips

---
+++ GameJam - The Disappointments/Assets/Scripts/TutorialTips.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTips : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialUI;
+    [SerializeField] private Text tutorialText;
+    
+    [TextArea]
+    public string tutorialTip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialText.text = tutorialTip;
+
+        tutorialUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !tutorialUI.activeSelf)
+        {
+            tutorialUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            tutorialUI.SetActive(false);
+        }
+    }
+}
